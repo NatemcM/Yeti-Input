8eca3b0510cf06289030009f3ba88e87dc905975
Nathanael McMillan <nmcmillan@perfectcolours.com>
2014-11-05 15:46:02 +0000

    Update Readme File

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-Yeti-Input
+Yeti Input
 ==========
 
 Yeti Input is a simple function which creates valid HTML inputs, helping make your code easier to read and faster to write. 
+
+## Creating Form Inputs
+
+Yeti Input supports a number parameters, these are:
+
+* Type: Type of input i.e. Text, Color, Tel, Email
+* Name: Name of the input (`<input name="$name">`)
+* Label: Description for the label i.e. Please enter your name
+* Class: Add a class to the input field 
+* Required: This field is required i.e. (`<input type="text" required>`)
+
+Creating input fields is as simple as:
+
+	<?= yeti_input('text', 'firstName', 'Please enter your first name', 'input text', 'r') ?>
+	
+This will output: 
+
+	<label>Please enter your first name
+	<input type="text" name="firstName" class="input text"  required />
+	</label>
+	
+### Creating Options
+
+Just like a text field, a select field can be created like this: 
+
+	<?= input('select', 'select', 'This: Is: A: Dropdown menu: Its nice you know'); ?>
+	
+Just separate the label param with a `:` (colon).
+ 
+This will output: 
+ 
+	<select name="select"> 
+		<option value="This">This</option> 
+		<option value="Is">Is</option>
+		<option value="A">A</option>
+		<option value="DropdownMenu">Dropdown menu</option>
+		<option value="ItsNiceYouKnow">Its nice you know</option>
+	</select> 
+	
+### HTML5
+
+Yeti Input also has limited support for HTML5 form elements such as Datalist 
+
+	<?= input('datalist', 'companies', 'Firefox: Internet Explorer: Chrome: Opera'); ?>
+	
+This will output:
+
+	<input list="companies">
+	<datalist id="companies">
+		<option value="Firefox">
+		<option value="Internet Explorer">
+		<option value="Chrome">
+		<option value="Opera">
+	</datalist>
+	
+### Submitting Forms
+
+Creating submit buttons is easy as anything: 
+
+	<?= input('submit', 'submit', 'This is a submit button'); ?>
+	
+This will output: 
+
+	<input type="submit" value="This is a submit button" name="submit"  />
+	
+# To Do
+
+* More support for HTML5 Elements
+* Add an ID param (without going overboard on the accepted parameters)
+* Possibly look into creating an all encompassing form builder like RORs
---
+++ yeti-input.php
@@ -0,0 +1,112 @@
+<?php
+/**
+* # Yeti Input
+* ## Simply create valid HTML input fields 
+*
+* Yeti Input is a simple function which creates valid 
+* HTML inputs, helping make your code easier to read 
+* and faster to write.
+* 
+* @author Nathanael McMillan
+* @version 0.1
+* @copyright The MIT License (MIT)
+
+Copyright (c) <2014> <Nathanael McMillan>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*
+*/
+
+function yeti_input($type = 'text', $name, $label, $class = null, $require = null) {
+	
+	if(!empty($require)){
+		$require = 'required';	
+	}
+	
+	if(!empty($class)) {
+		$class = "class=\"$class\" ";	
+	}
+	
+	if($type == 'submit') {
+		return <<<BUILD_OUTPUT
+		<input type="submit" value="$label" name="$name" $class $require/>
+		
+BUILD_OUTPUT;
+
+	} else
+			
+	if($type == 'select'){
+		$select = "<select name=\"$name\">\r\n";
+		
+		$optionGroup = explode(': ', $label); // Create and array from the submited values
+		
+			foreach($optionGroup as $optionValue) {
+				$valueName = str_replace(' ','', ucwords($optionValue)); // Creat Camel Case value name
+				$select .= "<option value=\"$valueName\">$optionValue</option>\r\n";
+			}
+		$select .= "</select>";
+		
+		return $select;
+	} else
+	
+	if($type == 'textarea') {
+		return <<<TEXT_AREA
+		<label>$label
+    		<textarea name="$name" $class $require></textarea>
+        </label>
+TEXT_AREA;
+	} else
+	
+	if($type == 'datalist') {
+		$dataList = "<input list=\"$name\">\r\n";
+		$dataList .= "<datalist id=\"$name\">\r\n";
+		$optionGroup = explode(': ', $label); // Create and array from the submited values
+		foreach($optionGroup as $optionValue) {
+				$dataList .= "<option value=\"$optionValue\">\r\n";
+			}
+		$dataList .= "</datalist> ";
+		
+		return $dataList;
+		
+	} else
+	
+	{
+	return <<<BUILD_INPUT
+	<label>$label
+	<input type="$type" name="$name" $class $require />	
+	</label>
+	
+BUILD_INPUT;
+
+	} 
+	
+}
+
+?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
